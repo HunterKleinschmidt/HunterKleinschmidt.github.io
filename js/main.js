6f75815ba45349578223f5204e4eac73cd99388e
Hunter Kleinschmidt <hunter@kleinstrength.com>
2025-04-01 11:24:14 -0500

    Wrapped smoothscroll in document.ready

--- js/main.js
+++ js/main.js
@@ -190,20 +190,18 @@
 	/*---------------------------------------------------- */
   	/* Smooth Scrolling
   	------------------------------------------------------ */
-  	$('.smoothscroll').on('click', function (e) {
-	 	
-	 	e.preventDefault();
-
-   	var target = this.hash,
-    	$target = $(target);
-
-    	$('html, body').stop().animate({
-       	'scrollTop': $target.offset().top
-      }, 800, 'swing', function () {
-      	window.location.hash = target;
-      });
-
-  	});  
+	  $(document).ready(function() {
+		$('.smoothscroll').on('click', function (e) {
+			e.preventDefault();
+			var target = this.hash,
+				$target = $(target);
+			$('html, body').stop().animate({
+				'scrollTop': $target.offset().top
+			}, 800, 'swing', function () {
+				window.location.hash = target;
+			});
+		});
+	});
   
 
    /*---------------------------------------------------- */
